0774d2b92aad42460e2c48798f00950f124fbc2b
Daria <dar.nagaj@gmail.com>
2022-09-28 23:19:14 +0300

    feat:forms have state

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -1,12 +1,36 @@
 import cl from './personalInfo.module.scss';
-import {Link, NavLink} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom';
+import { useState } from "react";
 
 const PersonalInfo = () => {
+
+    let schema = require('./../json/JSONSchema.json')
+
+    const [name, setName] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [birthday, setBirthday] = useState('');
+
+    const nameHandler=(e)=>{
+        setName(e.target.value)
+    }
+
+    const lastnameHandler=(e)=>{
+        setLastname(e.target.value)
+    }
+
+    const birthdayHandler=(e)=>{
+        setBirthday(e.target.value)
+    }
+
+
+
+
     return (
        <div className={cl.form}>
+
             <form>
-                <input className={cl.input}  name='name' type="text" placeholder="First Name"/>
-                <input className={cl.input} name='lastname' type="text" placeholder="Last Name"/>
+                <input className={cl.input} value={name} onChange={e=>nameHandler(e)}  name='name' type="text" placeholder="First Name"/>
+                <input className={cl.input} value={lastname} onChange={e=>lastnameHandler(e)}  name='lastname' type="text" placeholder="Last Name"/>
 
 
                 <div className={cl.radiocontainer}>
@@ -15,17 +39,28 @@ const PersonalInfo = () => {
                     <input className={cl.radio} id="female" type="radio" name="question"/>
                     <label className={cl.radiolabel} for="female">Female</label>
                     <input className={cl.radio} id="not" type="radio" name="question"/>
-                    <label className={cl.radiolabel} for="not">Not detected</label>
+                    <label className={cl.radiolabel} for="not">Other</label>
                 </div>
 
-                <select className={cl.select}>
+                <label className={cl.datelabel} for="select">Favorite ocean:</label>
+                <select id="select" className={cl.select}>
                     <option value="">Select...</option>
-                    <option value="A">Option A</option>
-                    <option value="B">Option B</option>
+                    <option value="A">{schema['ocean']['oneOf'][0]}</option>
+                    <option value="B">{schema['ocean']['oneOf'][1]}</option>
+                    <option value="C">{schema['ocean']['oneOf'][2]}</option>
+                    <option value="D">{schema['ocean']['oneOf'][3]}</option>
                 </select>
 
                 <label className={cl.datelabel} for="birth">Date of birth:</label>
-                <input className={cl.input} name='birtday' id="birth" type="date"/>
+                <input className={cl.input} value={birthday} onChange={e=>birthdayHandler(e)}  name='birtday' id="birth" type="date"/>
+
+
+                <div className={cl.checkboxes}>
+                    <input className={cl.checkbox} type="checkbox" name="option1" value="a1" checked/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][0]}</span>
+                    <input className={cl.checkbox} type="checkbox" name="option2" value="a2"/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][1]}</span>
+                    <input className={cl.checkbox} type="checkbox" name="option3" value="a3"/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][2]}</span>
+                    <input className={cl.checkbox} type="checkbox" name="option4" value="a4"/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][3]}</span>
+                </div>
                 
 
 
--- src/components/SignUpInfo/SignUpInfo.js
+++ src/components/SignUpInfo/SignUpInfo.js
@@ -1,35 +1,107 @@
 import cl from './SignUpInfo.module.scss';
-import { useState } from "react";
-import {Link, NavLink} from 'react-router-dom'
-
+import { useState, useEffect} from "react";
+import {Link, NavLink} from 'react-router-dom';
+import validator from 'validator';
+import InputMask from 'react-input-mask';
 
 const SignUpInfo = () => {
+    let schema = require('./../json/JSONSchema.json');
+    let phoneRe=new RegExp(schema['mobilePhone']['regExp']);
+    let emailRe=new RegExp(schema['email']['regExp']);
 
-    // const [phone, setPhone] = useState('');
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
-    // const [password2, setPassword2] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [passwordError, setPasswordError]=useState(false);
+    const [phoneError, setPhoneError]=useState(false);
+    const [emailError, setEmailError]=useState(false);
+    const [passwordDirty, setPasswordDirty]=useState(false);
+    const [formValid, setFormValid]=useState(false);
 
-    // const emailHandler=(e)=>{
+    
+    
 
-    //     setEmail(e.target.value)
+    const phoneHandler=(e)=>{
+        setPhone(e.target.value)
+        if (!!(e.target.value)!=schema['mobilePhone']['required']) { 
+            setPhoneError(true);
+        }
+        else if (!phoneRe.test(e.target.value.replace("(", "").replace(")", ""))) {
+            setPhoneError(true);
+        } else {
+            setPhoneError(false);
+        }
+        
+    }
 
-    // }
+    const emailHandler=(e)=>{
+        setEmail(e.target.value)
+        
+        if (!!(e.target.value)!=schema['email']['required']) { 
+            setEmailError(true);
+        }
+        else if (!emailRe.test(e.target.value)) {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+                   }
+    }
 
+    const passwordHandler=(e)=>{
+        setPassword(e.target.value);
+        setPasswordDirty(true);
+        console.log(e.target.value.length)
+        if (e.target.value.length<Number(schema['password']['minLength']) || e.target.value.length>Number(schema['password']['maxLength']) ) {
+            setPasswordError(false)
+            setFormValid(false)
+        }
+        else if(e.target.value=="" || password2 !== e.target.value ) {
+            setPasswordError(passwordError=>true)
+            setFormValid(false)
+        } else {
+        setPasswordError(passwordError=>false);
+        setFormValid(true);
+        }
+    }
 
+    const password2Handler=(e)=>{
+        setPassword2(e.target.value);
+        setPasswordDirty(true);
+        if (e.target.value.length<Number(schema['password']['minLength']) || e.target.value.length>Number(schema['password']['maxLength']) ) {
+            setFormValid(false)
+        }
+        else if(e.target.value=="" || password !== e.target.value ) {
+            setPasswordError(passwordError=>true)
+            setFormValid(false)
+        } else {
+        setPasswordError(passwordError=>false);
+        setFormValid(true);
+        }
+    }
 
+    useEffect(()=>{
+        if (phoneError || emailError || passwordError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    },[phoneError,emailError,passwordError])
 
-
+  
 
     return (
        <div>
             <div className={cl.form}>
-            <form>
-                <input className={cl.form}  name='mobilePhone' type="text" placeholder="Phone"/>
-                <input className={cl.form} name='email' type="email" placeholder="Email"/>
-                <input className={cl.form} name='password' type="password" placeholder="Password"/>
-                <input className={cl.form}  name='password' type="password" placeholder="Repeat password"/>
-                <button className={cl.button} type='submit'><NavLink end style={({isActive})=>({color:isActive?'#202082':'inherit'})} to="/personal">Next step</NavLink></button>
+            <form id="form1">
+                <InputMask mask="+375(99)9999999" className={cl.input} value={phone} onChange={e=>phoneHandler(e)} placeholder="Phone" maskplaceholder="Mobile phone" type="text" />
+                <input onChange={e=>emailHandler(e)} className={cl.input} value={email} name='email' type="email" placeholder="Email"/>
+                {passwordError && <div className={cl.error}>Passwords don't match</div>}
+                <input onChange={e=>passwordHandler(e)} className={cl.input} value={password} name='password' type="password" placeholder="Password"/>
+                {(passwordDirty&&passwordError) && <div className={cl.error}>Passwords don't match</div>}
+                <input onChange={e=>password2Handler(e)} className={cl.input} value={password2} name='password' type="password" placeholder="Repeat password"/>
+                <input  form="form1" disabled ={!formValid} name="submit" className={cl.button} value="Submit" type='submit'/>
+                <button className={cl.button}><NavLink disabled={!formValid} end style={({isActive})=>({color:isActive?'#202082':'inherit'})} to="/personal">Next step</NavLink></button>
             </form>
        </div>
        </div>
