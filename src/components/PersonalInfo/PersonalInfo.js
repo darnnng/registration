6f8fb65816ac9a6af8524dfa7a3a3b8c1db6999a
Daria <dar.nagaj@gmail.com>
2022-09-29 14:41:56 +0300

    slight changes

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -2,8 +2,11 @@ import cl from './personalInfo.module.scss';
 import {Link, NavLink} from 'react-router-dom';
 import { useState, useEffect } from "react";
 import Modal from '../modal/Modal';
+import { RadioGroup, RadioButton } from 'react-radio-buttons';
 
 const PersonalInfo = () => {
+
+    
 
     let schema = require('./../json/JSONSchema.json')
 
@@ -17,6 +20,8 @@ const PersonalInfo = () => {
     const [modalActive,setModalActive]=useState(false)
     const [select,setSelect]=useState("")
     const [selectError, setSelectError]=useState(false);
+    const [radio,setRadio]=useState("")
+    const [check,setCheck]=useState(false)
 
     const nameHandler=(e)=>{
         setName(e.target.value)
@@ -63,6 +68,14 @@ const PersonalInfo = () => {
         }
     }
 
+    const radioHandler=(e)=>{
+        setRadio(e.target.value)
+    }
+
+    const checkHandler=(e)=>{
+        setCheck(!check)
+    }
+
     useEffect(()=>{
         if (nameError || lastnameError || birthdayError || selectError) {
             setFormValid(false)
@@ -72,8 +85,8 @@ const PersonalInfo = () => {
     },[nameError,lastnameError,birthdayError,selectError])
 
 
-
     return (
+        
        <div className={cl.form}>
 
             <form>
@@ -81,14 +94,15 @@ const PersonalInfo = () => {
                 <input className={cl.input} value={lastname} onChange={e=>lastnameHandler(e)}  name='lastname' type="text" placeholder="Last Name"/>
 
 
-                <div className={cl.radiocontainer}>
-                    <input className={cl.radio} id="male" type="radio" name="question"/>
+                <RadioGroup onChange={e=>radioHandler(e)} value={radio} className={cl.radiocontainer}>
+                    <label className={cl.radiolabel} value="select">Your sex:</label>
+                    <input  value="Male" className={cl.radio} id="male" type="radio" name="question"/>
                     <label className={cl.radiolabel} for="male">Male</label>
-                    <input className={cl.radio} id="female" type="radio" name="question"/>
+                    <input  value="Female" className={cl.radio} id="female" type="radio" name="question"/>
                     <label className={cl.radiolabel} for="female">Female</label>
-                    <input className={cl.radio} id="not" type="radio" name="question"/>
+                    <input  value="Other" className={cl.radio} id="not" type="radio" name="question"/>
                     <label className={cl.radiolabel} for="not">Other</label>
-                </div>
+                </RadioGroup>
 
                 <label className={cl.datelabel} for="select">Favorite ocean:</label>
                 <select id="select" value={select} onChange={e=>selectHandler(e)} className={cl.select}>
@@ -103,11 +117,11 @@ const PersonalInfo = () => {
                 <input className={cl.input} value={birthday} onChange={e=>birthdayHandler(e)}  name='birtday' id="birth" type="date"/>
 
 
-                <div className={cl.checkboxes}>
-                    <input className={cl.checkbox} type="checkbox" name="option1" value="a1"/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][0]}</span>
-                    <input className={cl.checkbox} type="checkbox" name="option2" value="a2"/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][1]}</span>
-                    <input className={cl.checkbox} type="checkbox" name="option3" value="a3"/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][2]}</span>
-                    <input className={cl.checkbox} type="checkbox" name="option4" value="a4"/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][3]}</span>
+                <div className={cl.checkboxes} value={check}>
+                    <input className={cl.checkbox} onChange={checkHandler} type="checkbox" name="option1" value={schema['hobby']['anyOf'][0]}/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][0]}</span>
+                    <input className={cl.checkbox} onChange={checkHandler} type="checkbox" name="option2" value={schema['hobby']['anyOf'][1]}/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][1]}</span>
+                    <input className={cl.checkbox} onChange={checkHandler} type="checkbox" name="option3" value={schema['hobby']['anyOf'][2]}/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][2]}</span>
+                    <input className={cl.checkbox} onChange={checkHandler} type="checkbox" name="option4" value={schema['hobby']['anyOf'][3]}/><span className={cl.checkboxspan}>{schema['hobby']['anyOf'][3]}</span>
                 </div>
                 
 
@@ -117,14 +131,14 @@ const PersonalInfo = () => {
                
                 
             </form>
-            <Modal active={modalActive} setActive={setModalActive} name={name} lastname={lastname} birth={birthday} select={select}>
+            <Modal active={modalActive} setActive={setModalActive} name={name} lastname={lastname} birth={birthday} select={select} radio={radio}>
                 <div className={cl.modp}>Questionnaire</div>
                 <div className={cl.mod}>First name: {name} </div>
                 <div className={cl.mod}>Last name: {lastname}</div>
-                <div className={cl.mod}>Sex: </div>
+                <div className={cl.mod}>Sex: {radio} </div>
                 <div className={cl.mod}>Birthday: {birthday}</div>
                 <div className={cl.mod}>Favourite ocean: {select} </div>
-                <div className={cl.mod}>Hobbies: </div>
+                <div className={cl.mod}>Hobbies:{check} </div>
                 
 
              </Modal>
